3d3cf082c0af4d1954abe4e3f44561cae84179af
ZhansayaM <maksutzh@gmail.com>
2021-05-29 02:31:33 +0600

    types of data, variables, operators

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 1000000;
+let income = 'freelance';
+let addExpenses = 'room, wifi, food, taxi';
+let deposit = true;
+let mission = 5e10;
+let period = 12;
 
-alert("this is my first task!");
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев.\nЦель заработать ' + mission + ' долларов');
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
 
-console.log("This is a text for the console!");
+let budgetDay = money / 30;
+console.log(budgetDay);
+// alert("this is my first task!");
+
+// console.log("This is a text for the console!");
