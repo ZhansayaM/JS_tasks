db3b1d6aa3afa942142e37459c2fa4b1189fd57f
ZhansayaM <maksutzh@gmail.com>
2021-05-29 22:37:36 +0600

    harder tasks solutions

---
+++ script.js
@@ -0,0 +1,29 @@
+let num = 266219;                                   //task1
+
+function digitMultiplication(num) {
+    let multiply = 1;
+    while(Math.trunc(num) > 0) {
+        mod = num % 10;
+        num /= 10;
+        num = Math.trunc(num);
+        multiply *= mod;
+    }
+    return multiply;
+}
+
+console.log(digitMultiplication(num));              //task2
+
+let exponential = digitMultiplication(num) ** 3;    //task3
+
+function firstNDigits(num, n) {
+    n = Math.pow(10, n);
+    while (num > n) {
+        num /= 10;
+        num = Math.trunc(num);
+    }
+    return num;
+}
+
+console.log(firstNDigits(exponential, 2));          //task4, var1
+
+console.log(String(exponential).substr(0,2));       //task4, var2
