b67cf026d1c29bf2d472b3b2854b625de0da795d
ZhansayaM <maksutzh@gmail.com>
2021-06-04 00:40:32 +0600

    lesson06 harder solutions

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 //Game bot 'Загадывание случайного числа от 1 до 100'
 function userNumber() {
-  let randNumber = Math.floor(Math.random() * 100 + 1);
-  console.log(randNumber);
-  function inner() {
-    let userInput = prompt("Угадай число от 1 до 100");
-    if (userInput === null) {
-      alert("Игра окончена");
-    } else if (Number.isNaN(+userInput) || userInput.length === 0) {
-      alert("Введи число!");
-      inner();
-    } else if (+userInput > randNumber) {
-      alert("Загаданное число меньше");
-      inner();
-    } else if (+userInput < randNumber) {
-      alert("Загаданное число больше");
-      inner();
-    } else if (+userInput === randNumber) {
-      alert("Поздравляю, Вы угадали!!!");
+    let count = 10;
+    let randNumber = Math.floor(Math.random() * 100 + 1);
+    console.log(randNumber);
+    function inner() {
+      let userInput = prompt("Угадай число от 1 до 100");
+      if (count <= 1) {
+        if (confirm("Попытки закончились, хотите сыграть еще?")) userNumber();
+      } else if (userInput === null) {
+        alert("Игра окончена");
+      } else if (Number.isNaN(+userInput) || userInput.length === 0) {
+        alert("Введи число!");
+        inner();
+      } else if (+userInput > randNumber) {
+        count--;
+        alert("Загаданное число меньше, осталось попыток " + count);
+        inner();
+      } else if (+userInput < randNumber) {
+        count--;
+        alert("Загаданное число больше, осталось попыток " + count);
+        inner();
+      } else if (+userInput === randNumber) {
+        if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) userNumber();
+      }
     }
+    return inner;
   }
-  return inner;
-}
-
-let getResult = userNumber();
-getResult();
-console.dir(getResult);
+  
+  let getResult = userNumber();
+  getResult();
+  console.dir(getResult);
