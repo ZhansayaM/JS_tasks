847b70e65553537611b920b320aa9d74bc1b4085
ZhansayaM <maksutzh@gmail.com>
2021-06-05 05:11:46 +0600

    bold italics clash fixed

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 for (let item of week){
-  if (item === 'Saturday' || item === 'Sunday') item = item.italics();
-  let day = new Date().getDay();
-  if (item === week[day-1]) item = item.bold();
+  let bold = 0, italics = 0, day = new Date().getDay();
+  if (item === week[day-1]) bold = 1;
+  if (item === 'Saturday' || item === 'Sunday') italics = 1;
+  if (bold === 1) item = item.bold();
+  if (italics === 1) item = item.italics();
+  document.write(item + '<br>');
   console.log(item);
 }
