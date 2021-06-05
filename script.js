b9f328a6a6aa6f1c974ca98db8c2a9257df9e7eb
ZhansayaM <maksutzh@gmail.com>
2021-06-06 01:34:22 +0600

    bold, italics fixed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let day = new Date().getDay();
 
 for (let item of week){
-  let bold = 0, italics = 0, day = new Date().getDay();
-  if (item === week[day-1]) bold = 1;
+let bold = 0, italics = 0;
+  if (item === week[day]) bold = 1;
   if (item === 'Saturday' || item === 'Sunday') italics = 1;
-  if (bold === 1) item = item.bold();
-  if (italics === 1) item = item.italics();
+  if (bold === 1) item = '<b>' + item + '</b>';
+  if (italics === 1) item = '<i>' + item + '</i>';
   document.write(item + '<br>');
-  console.log(item);
 }
