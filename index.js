da18ad44816fe94f62fa63a173375e34ffda7982
ZhansayaM <maksutzh@gmail.com>
2021-06-11 01:19:03 +0600

    lesson09 solutons

---
+++ index.js
@@ -0,0 +1,12 @@
+const count = document.getElementById('start');
+const plusIncome = document.getElementsByTagName('button')[0];
+const plusExpense = document.getElementsByTagName('button')[1];
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncome = document.querySelectorAll('.additional_income-item');
+const rightSideValues = document.querySelectorAll('[class$="-value"]');
+const allInputs = document.querySelectorAll('input');
+let inputs = [];
+for (let i = 0; i < 13; i++){
+    inputs[i] = allInputs[i];
+    console.log(inputs[i]);
+}
